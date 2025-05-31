96563fa48fa56c196db33471bfe91f2fbd78c526
bsede <bsede84@gmail.com>
2025-05-31 17:15:11 -0500

    bookbot now prints a report

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
+from stats import num_words
+from stats import chars
+from stats import report
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-from stats import num_words
-
 def main():
     book_path = "books/frankenstein.txt"
     book_text = get_book_text(book_path)
     book_words = num_words(book_text)
+    book_chars = chars(book_text)
+    book_report = report(book_chars)
 
-    print(f"{book_words} words found in the document")
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {book_words} total words")
+    print("--------- Character Count -------")
+    for entry in book_report:
+        if entry["char"].isalpha():
+            print(f"{entry["char"]}: {entry["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -4,4 +4,23 @@ def num_words(text):
     return text_length
 
 def chars(text):
-    pass
+    char_counts = {}
+    for char in text.lower():
+        if char in char_counts:
+            char_counts[char] += 1
+        else:
+            char_counts[char] = 1
+    return char_counts
+
+def report(dict):
+    report_list = []
+    for entry in dict:
+        count = dict[entry]
+        report_dict = {"char": entry, "num": count}
+        report_list.append(report_dict)
+    
+    report_list.sort(reverse=True, key=sort_on)
+    return report_list
+
+def sort_on(dict):
+    return dict["num"]
